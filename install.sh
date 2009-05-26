be64ced1bf780069571f1d8d40a827f2558c96f8
Kai Chen <chenk85@gmail.com>
2009-05-26 18:47:19 +0800

    update install script and update ruby.snippets

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ if [ -d ~/.vim ]; then
     if [ -f ~/.vim/plugin/snipMate.vim ]; then
         cp ./*.snippets ~/.vim/snippets/
         echo "Installed snippets successfully :)"
+    else
+        echo "Please install snipMate vim plugin first!"
     fi
 else
-    echo "Please install snipMate vim plugin first!"
+    echo "Please install Vim first!"
 fi
